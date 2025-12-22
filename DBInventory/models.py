e45921db5f97503dfe7142ece5cbc30ad0eb34cd
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-12-21 20:48:10 -0600

    adding another files 21/12/2025

--- DBInventory/models.py
+++ DBInventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 
-# Create your models here.
+# Here we have the models of the project, that in this case are dynamic models.
+# Basically with a dynamic model a user can creates different tables on the db without affecting the main structure of this one.
+
+# in this class we're creating the user is gonna create they tables.
+class InventoryTypes_Tables(models.Model):
+    name = models.CharField(max_length=100)
+    table = JSONField()
+    
+
+# in this class the user is gonna create base on the colums that this one added to his new tables the items for each of this columns.
+class InventoryItems(models.Model):
+    name = models.ForeignKey(InventoryTypes_Tables, on_delete=models.CASCADE)
+    data = JSONField()
---
+++ DBInventory/serializer.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from .models import *
+
+#This is the serializer that the model needs to transform the data of the settings from sql data to json api
+class InventoryTypes_Tables_Serializer(serializers.ModelSerializer):
+    # validation function of the tables that are creating
+    def validate_tables(self, value):
+        allowed_types = {"string", "number", "created_at"}
+        fields = value.get("fieds", {})
+        
+        if not fields:
+            raise serializers.ValidationError("The table must have at least one field.")
+        
+        for field_name, field_type in fields.items():
+            if field_type not in allowed_types:
+                raise serializers.ValidationError(
+                    f"Invalid type '{field_type}' for field '{field_name}'"
+                )
+        return value
+
+    class Meta:  
+        model = InventoryTypes_Tables
+        fields = "__all__"
+        
+        
+class IventoryItems_Serializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kargs):
+        super().__init__(*args, **kargs)
+        
+        # attrs = attributes
+        def validate(self, attrs):
+            if not attrs:
+                raise serializers.ValidationError(
+                    "No valid fields provided for this table."
+                )
+            return attrs
+        
+        table_instance = self.context.get('table_instance')
+        
+        if table_instance:
+            for field_name, field_type in table_instance['fields'].items():
+                if field_type == 'string':
+                    self.fields[field_name] = serializers.CharField()
+                elif field_type == 'number':
+                    self.fields[field_name] = serializers.IntegerField()
+                elif field_type == 'created_at':
+                    self.fields[field_name] = serializers.DateTimeField()
+        
+        def create(self, validated_data):
+            table = self.context['table_model']
+            return InventoryItems.objects.create(
+                table_ref = table,
+                data = validated_data
+            )
+            
+        def to_representation(self, table_instance):
+            return table_instance.data 
